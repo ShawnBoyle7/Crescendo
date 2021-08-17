6cf7407ae5ab702fc02d7bf0d071c7162a28b79d
Shawn Boyle <shawn.boyle01@gmail.com>
2021-08-16 23:27:58 -0600

    before refactoring redux

--- frontend/src/components/Artists/index.js
+++ frontend/src/components/Artists/index.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { getArtists } from "../../store/artists";
 import { useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 const Artists = () => {
   const dispatch = useDispatch();
@@ -16,8 +17,7 @@ const Artists = () => {
     <>
       <h1>This is the artists component</h1> 
       <ul>
-        {artists.map(artist =>  <li key={artist.id}>{artist.name}</li>
-        )}
+        {artists.map(artist => <li key={artist.id}> <Link to={`/artists/${artist.id}`}> {artist.name} </Link></li>)}
       </ul>
     </>
   )

7c69db513c69cf5b2666fed93bcaf57f0a548310
Shawn Boyle <shawn.boyle01@gmail.com>
2021-08-18 10:21:55 -0600

    popularity sorting on home

--- backend/routes/api/artists.js
+++ backend/routes/api/artists.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const { Artist, Genre, Album } = require('../../db/models');
+const { Artist, Genre, Album, User, Song } = require('../../db/models');
 const router = express.Router();
 
 router.get('/', asyncHandler(async (req, res) => {
   const artists = await Artist.findAll({
     include: [
       Genre,
-      Album
+      Album,
+      User,
+      Song
     ]
   });
 

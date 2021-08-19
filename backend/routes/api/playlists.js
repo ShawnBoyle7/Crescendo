03d258818990c827c147e02675999583bca5ca55
Shawn Boyle <shawn.boyle01@gmail.com>
2021-08-19 12:12:12 -0600

    starting playlist CRUD

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const { Playlist, Song, Artist } = require('../../db/models');
+const { Playlist, Song, Artist, Album } = require('../../db/models');
 const router = express.Router();
 
 router.get('/', asyncHandler(async (req, res) => {
   const playlists = await Playlist.findAll();
 
   return res.json(playlists)
+}));
+
+
+router.post('/', asyncHandler(async (req, res) => {
+  const { name } = req.body
+  // const userId = 
+
 }));
 
 module.exports = router;

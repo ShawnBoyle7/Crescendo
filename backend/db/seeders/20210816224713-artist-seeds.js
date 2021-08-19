5f66b9e8bc325a14539e74e9bf7b491bdc33bdf5
Shawn Boyle <shawn.boyle01@gmail.com>
2021-08-18 20:24:32 -0600

    css structuring on home

--- backend/db/seeders/20210816224713-artist-seeds.js
+++ backend/db/seeders/20210816224713-artist-seeds.js
@@ -4,7 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Artists', 
     [
-      { name: "Shane Ivers", genreId: 1, createdAt: new Date(), updatedAt: new Date() },
+      { name: "Shane Ivers", genreId: 1, artistImgUrl: "imgur.com/KPY4ORA.png", createdAt: new Date(), updatedAt: new Date() },
       { name: "Benjamin Tissot", genreId: 2, createdAt: new Date(), updatedAt: new Date() },
       { name: "Mr. Ryan", genreId: 3, createdAt: new Date(), updatedAt: new Date() },
       { name: "Alexander Nakarada", genreId: 4, createdAt: new Date(), updatedAt: new Date() },

eda49d6b1e9746d29faf9c2735199d6f28b697ee
Shawn Boyle <shawn.boyle01@gmail.com>
2021-08-19 19:51:37 -0600

    library data variables complete

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const { Playlist, Song, Artist, Album } = require('../../db/models');
+const { Playlist } = require('../../db/models');
 const router = express.Router();
 
 router.get('/', asyncHandler(async (req, res) => {

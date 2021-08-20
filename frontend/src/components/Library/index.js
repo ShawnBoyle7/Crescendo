c68217f24bfa80896cb1c35971c10183770d86a9
Shawn Boyle <shawn.boyle01@gmail.com>
2021-08-19 20:46:19 -0600

    library data links

--- frontend/src/components/Library/index.js
+++ frontend/src/components/Library/index.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Library = () => {
   const sessionUser = useSelector(state => state.session.user);
@@ -7,14 +8,23 @@ const Library = () => {
   const playlistsSlice = useSelector(state => state.playlists);
   const allPlaylists = Object.values(playlistsSlice);
 
-  // Library Data
   const userPlaylists = allPlaylists.filter(playlist => playlist.userId === +user.id);
   const followedArtists = user?.Artists;
   const likedAlbums = user?.Albums
 
   return(
     <>
-      
+      <div className="playlists-div">
+      {userPlaylists ? userPlaylists.map(playlist => <div className="playlist-links" key={playlist.id}><Link to={`/playlists/${playlist.id}`}>{playlist.name}</Link></div>) : <></>}
+      </div>
+
+      <div className="artists-div">
+      {followedArtists ? followedArtists.map(artist => <div className="artist-links" key={artist.id}><Link to={`/artists/${artist.id}`}>{artist.name}</Link></div>) : <></>}
+
+      </div>
+      <div className="albums-div">
+      {likedAlbums ? likedAlbums.map(album => <div className="album-links" key={album.id}><Link to={`/albums/${album.id}`}>{album.name}</Link></div>) : <></>}
+      </div>
     </>
   )
 }

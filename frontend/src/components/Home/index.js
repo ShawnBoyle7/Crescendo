2864389c0896eb1efcf10f2be51706e379a836d1
Shawn <shawn.boyle01@gmail.com>
2021-08-21 01:17:54 -0600

    home page styling structure

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -21,19 +21,33 @@ const Home = () => {
       {/* <h1 className="welcome-message">Welcome!</h1> */}
 
       <div className="artists-section">
-      <div className="artist-divs">
       <h1 className="popular-artists">Popular Artists</h1>
-        {artistsByPopularity.map(artist => <div className="artist-item" key={artist.id}> <Link to={`/artists/${artist.id}`}><div className="artist-name">{artist.name}</div> <img className="artists-image" src={artist.artistImgUrl} alt="Artist"/> </Link></div>)}
+      <div className="artists-divs">
+        {artistsByPopularity.map(artist => 
+          <div className="artists-item" key={artist.id}> 
+          <Link to={`/artists/${artist.id}`}>
+          <img className="artists-image" src={artist.artistImgUrl} alt="Artist"/>
+          <div className="artists-name">{artist.name}</div>  
+          </Link></div>)}
       </div>
-      <Link to="/artists"><div className="all-artists">Browse All Artists</div></Link>
+      <div className="all-artists">
+        <Link to="/artists">Browse All Artists</Link>
+      </div>   
       </div>
 
       <div className="songs-section">
+      <h1 className="popular-songs">Popular Songs</h1>
       <div className="song-divs">
-        <h1 className="popular-songs">Popular Songs</h1>
-          {songsByPopularity.map(song => <div className="song-item" key={song.id}> <Link to={`/songs/${song.id}`}> <div className="song-name">{song.name}</div> <img className="songs-image" src={song.songImgUrl} alt="Song"/> </Link></div>)}
+        {songsByPopularity.map(song =>
+          <div className="songs-item" key={song.id}>
+          <Link to={`/songs/${song.id}`}>
+          <img className="songs-image" src={song.songImgUrl} alt="Song"/> 
+          <div className="songs-name">{song.name}</div>
+          </Link></div>)}
       </div>
-        <Link to="/songs"><div className="all-songs">Browse All Songs</div></Link>
+      <div className="all-songs">
+        <Link to="/songs">Browse All Songs</Link>
+      </div>    
       </div>
     </div>
   )

97d850048608dd195a951a23afb6631fac463e09
Shawn Boyle <shawn.boyle01@gmail.com>
2021-10-17 20:19:24 -0600

    genre seeds

--- backend/db/seeders/20210816223418-genre-seeds.js
+++ backend/db/seeders/20210816223418-genre-seeds.js
@@ -2,13 +2,17 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Genres', 
+    return queryInterface.bulkInsert('Genres',
     [
-      { name: "Piano", createdAt: new Date(), updatedAt: new Date() },
-      { name: "Country", createdAt: new Date(), updatedAt: new Date() },
-      { name: "Jazz", createdAt: new Date(), updatedAt: new Date() },
       { name: "Rock", createdAt: new Date(), updatedAt: new Date() },
-      { name: "Pop", createdAt: new Date(), updatedAt: new Date() }
+      { name: "Pop", createdAt: new Date(), updatedAt: new Date() },
+      { name: "Hip Hop", createdAt: new Date(), updatedAt: new Date() },
+      { name: "Metal", createdAt: new Date(), updatedAt: new Date() },
+      { name: "Anisong", createdAt: new Date(), updatedAt: new Date() },
+      { name: "Punk Rock", createdAt: new Date(), updatedAt: new Date() },
+      { name: "Southern Rock", createdAt: new Date(), updatedAt: new Date() },
+      { name: "Progressive Rock", createdAt: new Date(), updatedAt: new Date() },
+      { name: "Alternative Rock", createdAt: new Date(), updatedAt: new Date() },
     ])
   },
   down: (queryInterface, Sequelize) => {

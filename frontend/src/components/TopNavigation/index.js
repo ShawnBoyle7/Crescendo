54174b68a6a76d81179a9b3541ec254031ced759
Shawn Boyle <shawn.boyle01@gmail.com>
2021-10-26 16:23:40 -0600

    back and forward buttons functional

--- frontend/src/components/TopNavigation/index.js
+++ frontend/src/components/TopNavigation/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './TopNavigation.css';
 
 const TopNavigation = () => {
+    const history = useHistory();
     
     return (
         <>
             <nav className="top-navigation-bar">
-                <div className="top-nav-elements">
-                    <div className="history-div">
-                        <span className="history-span"><i class="fas fa-chevron-left"></i></span>
-                        <span className="history-span"><i class="fas fa-chevron-right"></i></span>
+                <div className="top-nav-features">
+                    <div className="history-buttons-div">
+                        <button className="history-button" onClick={() => history.go(-1)}><i class="fas fa-chevron-left"></i></button>
+                        <button className="history-button" onClick={() => history.go(1)}><i class="fas fa-chevron-right"></i></button>
                     </div>
                 </div>
                 <div className="user-dropdown">

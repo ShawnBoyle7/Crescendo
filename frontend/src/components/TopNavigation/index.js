ba84c10b6d0668c4ad1e865c63cac77bc2c654cf
Shawn Boyle <shawn.boyle01@gmail.com>
2021-10-26 16:36:39 -0600

    navbar scrolling claass change functional

--- frontend/src/components/TopNavigation/index.js
+++ frontend/src/components/TopNavigation/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import ProfileButton from './ProfileButton';
 import './TopNavigation.css';
 
 const TopNavigation = () => {
     const history = useHistory();
+
+    document.addEventListener("scroll", () => {
+        const nav = document.querySelector("nav")
+    
+        if (window.scrollY === 0) {
+            nav?.classList.add("top-navigation-bar-default")
+            nav?.classList.remove("top-navigation-bar-scrolled")
+        } else if (window.scrollY > 0) {
+            nav?.classList.remove("top-navigation-bar-default")
+            nav?.classList.add("top-navigation-bar-scrolled")
+        }
+    })
     
     return (
         <>
-            <nav className="top-navigation-bar">
+            <nav className="top-navigation-bar-default">
                 <div className="top-nav-features">
                     <div className="history-buttons-div">
                         <button className="history-button" onClick={() => history.go(-1)}><i class="fas fa-chevron-left"></i></button>

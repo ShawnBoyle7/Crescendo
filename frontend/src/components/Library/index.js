016d7933e815736492ff25eebbc4614f6d9ec457
Shawn Boyle <shawn.boyle01@gmail.com>
2021-11-07 13:40:30 -0700

    playlist div data set

--- frontend/src/components/Library/index.js
+++ frontend/src/components/Library/index.js
@@ -1,7 +1,8 @@
 import { useSelector } from "react-redux";
-import { Link, Route, Switch } from "react-router-dom";
-import { ArtistDiv } from "../ArtistDiv"
-import { AlbumDiv } from "../AlbumDiv"
+import { Route, Switch } from "react-router-dom";
+import ArtistDiv from "../ArtistDiv"
+import AlbumDiv from "../AlbumDiv"
+import PlaylistDiv from "../PlaylistDiv";
 import './Library.css'
 
 const Library = () => {
@@ -22,43 +23,24 @@ const Library = () => {
                     <Route path="/library/playlists">
                         <div className="playlists-section">
                             {userPlaylists ? userPlaylists.map(playlist =>
-                                <div className="playlists-item" key={playlist.id}>
-                                    <Link to={`/playlists/${playlist.id}`}>
-                                        <img className="playlists-image" alt={"playlist"} src={ playlist.Songs.length ? playlist.Songs[0].Album.imgUrl : "https://i.imgur.com/KKVhCBg.png"}/>
-                                        <div className="playlists-name">{playlist.name}</div>
-                                    </Link>
-                                </div>)
-                                : <></>}
+                                <PlaylistDiv playlist={playlist} sessionUser={sessionUser}/>)
+                            : <></>}
                         </div>
                     </Route>
 
                     <Route path="/library/artists">
-                        <div className="artist-section">
-                            <div className="artist-section">
-                                {followedArtists ? followedArtists.map(artist =>
-                                    <div className="artists-item" key={artist.id}>
-                                        <Link to={`/artists/${artist.id}`}>
-                                            <img className="artists-image" alt={"artist"} src={artist.artistImgUrl} />
-                                            <div className="artists-name">{artist.name}</div>
-                                        </Link>
-                                    </div>)
-                                    : <></>}
-                            </div>
+                        <div className="artists-section">
+                            {followedArtists ? followedArtists.map(artist =>
+                                <ArtistDiv artist={artist}/>)
+                            : <></>}
                         </div>
                     </Route>
 
                     <Route path="/library/albums">
                         <div className="albums-section">
-                            <div className="album-section">
-                                {likedAlbums ? likedAlbums.map(album =>
-                                    <div className="albums-item" key={album.id}>
-                                        <Link to={`/albums/${album.id}`}>
-                                            <img className="albums-image" alt={"album"} src={album.albumImgUrl} />
-                                            <div className="albums-name">{album.name}</div>
-                                        </Link>
-                                    </div>)
-                                : <></>}
-                            </div>
+                            {likedAlbums ? likedAlbums.map(album =>
+                                <AlbumDiv album={album}/>)
+                            : <></>}
                         </div>
                     </Route>
                 </Switch>
---
+++ frontend/src/components/PlaylistDiv/index.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import "./PlaylistDiv.css"
+
+const PlaylistDiv = ({ playlist, sessionUser }) => {
+    return (
+        <>
+            <Link to={`/playlists/${playlist?.id}`}>
+                <div className="playlist-card" key={playlist?.id}>
+                    <img className="playlist-image" src={playlist?.Songs[0]?.Album.imgUrl} alt="playlist-art"/>
+                    <span className="playlist-name">{playlist?.name}</span>
+                        <span className="playlist-creator">
+                            By {sessionUser.username}
+                        </span>
+                </div>
+            </Link>
+        </>
+    )
+}
+
+export default PlaylistDiv;

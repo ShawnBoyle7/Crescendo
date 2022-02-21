e5dfd6ac1d57640d784f92ab1a1477a4bab98309
Shawn Boyle <shawn.boyle01@gmail.com>
2022-02-20 23:01:24 -0700

    all artist content seeded

--- backend/db/seeders/20210816231055-song-seeds.js
+++ backend/db/seeders/20210816231055-song-seeds.js
@@ -9,7 +9,7 @@ module.exports = {
         name: 'Norwegian Wood (This Bird Has Flown)', artistId: 1, albumId: 1, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/rubber-soul/rubber+soul/Norwegian+Wood+(This+Bird+Has+Flown).mp3', songLength: '2:04', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'You Won\'t See Me', artistId: 1, albumId: 1, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/rubber-soul/rubber+soul/You+Won't+See+Me+(Remastered+2009).mp3", songLength: '3:19', createdAt: new Date(), updatedAt: new Date(),
+        name: 'You Won\'t See Me', artistId: 1, albumId: 1, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/rubber-soul/rubber+soul/You+Won\'t+See+Me+(Remastered+2009).mp3', songLength: '3:19', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'Nowhere Man', artistId: 1, albumId: 1, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/rubber-soul/rubber+soul/Nowhere+Man+(Remastered+2009).mp3', songLength: '2:43', createdAt: new Date(), updatedAt: new Date(),
@@ -30,7 +30,7 @@ module.exports = {
         name: 'Girl', artistId: 1, albumId: 1, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/rubber-soul/rubber+soul/Girl+(Remastered+2009).mp3', songLength: '2:31', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'I\'m Looking Through You', artistId: 1, albumId: 1, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/rubber-soul/rubber+soul/I'm+Looking+Through+You+(Remastered+2009).mp3", songLength: '2:26', createdAt: new Date(), updatedAt: new Date(),
+        name: 'I\'m Looking Through You', artistId: 1, albumId: 1, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/rubber-soul/rubber+soul/I\'m+Looking+Through+You+(Remastered+2009).mp3', songLength: '2:26', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'In My Life', artistId: 1, albumId: 1, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/rubber-soul/rubber+soul/In+My+Life+(Remastered+2009).mp3', songLength: '2:26', createdAt: new Date(), updatedAt: new Date(),
@@ -52,7 +52,7 @@ module.exports = {
         name: 'Sparks Fly', artistId: 2, albumId: 2, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/speak-now/speak-now/Sparks+Fly.mp3', songLength: '4:20', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Back To December', artistId: 2, albumId: 2, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/speak-now/speak-now/Back+To+December.mp3", songLength: '4:53', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Back To December', artistId: 2, albumId: 2, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/speak-now/speak-now/Back+To+December.mp3', songLength: '4:53', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'Speak Now', artistId: 2, albumId: 2, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/speak-now/speak-now/Speak+Now.mp3', songLength: '4:00', createdAt: new Date(), updatedAt: new Date(),
@@ -73,7 +73,7 @@ module.exports = {
         name: 'Enchanted', artistId: 2, albumId: 2, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/speak-now/speak-now/Enchanted.mp3', songLength: '5:52', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Better Than Revenge', artistId: 2, albumId: 2, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/speak-now/speak-now/Better+Than+Revenge.mp3", songLength: '3:37', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Better Than Revenge', artistId: 2, albumId: 2, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/speak-now/speak-now/Better+Than+Revenge.mp3', songLength: '3:37', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'Innocent', artistId: 2, albumId: 2, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/speak-now/speak-now/Innocent.mp3', songLength: '5:02', createdAt: new Date(), updatedAt: new Date(),
@@ -95,10 +95,10 @@ module.exports = {
         name: 'For Free? - Interlude', artistId: 3, albumId: 3, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/to-pimp-a-butterfly/For+Free+Interlude+-+Kendrick+Lamar+(To+Pimp+a+Butterfly).mp3', songLength: '2:10', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: "King Kunta", artistId: 3, albumId: 3, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/to-pimp-a-butterfly/King+Kunta.mp3", songLength: '3:54', createdAt: new Date(), updatedAt: new Date(),
+        name: 'King Kunta', artistId: 3, albumId: 3, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/to-pimp-a-butterfly/King+Kunta.mp3', songLength: '3:54', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Institutionalized', artistId: 3, albumId: 3, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/to-pimp-a-butterfly/Institutionalized.mp3", songLength: '4:31', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Institutionalized', artistId: 3, albumId: 3, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/to-pimp-a-butterfly/Institutionalized.mp3', songLength: '4:31', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'These Walls', artistId: 3, albumId: 3, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/to-pimp-a-butterfly/These+Walls.mp3', songLength: '5:00', createdAt: new Date(), updatedAt: new Date(),
@@ -119,7 +119,7 @@ module.exports = {
         name: 'Hood Politics', artistId: 3, albumId: 3, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/to-pimp-a-butterfly/Hood+Politics.mp3', songLength: '4:52', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'How Much A Dollar Cost', artistId: 3, albumId: 3, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/to-pimp-a-butterfly/How+Much+A+Dollar+Cost.mp3", songLength: '4:21', createdAt: new Date(), updatedAt: new Date(),
+        name: 'How Much A Dollar Cost', artistId: 3, albumId: 3, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/to-pimp-a-butterfly/How+Much+A+Dollar+Cost.mp3', songLength: '4:21', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'Complexion (A Zulu Love)', artistId: 3, albumId: 3, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/to-pimp-a-butterfly/Complexion+(A+Zulu+Love).mp3', songLength: '4:23', createdAt: new Date(), updatedAt: new Date(),
@@ -144,7 +144,7 @@ module.exports = {
         name: 'CALLING', artistId: 4, albumId: 4, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/microcosm/Calling.mp3', songLength: '3:34', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Sign', artistId: 4, albumId: 4, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/microcosm/Sign.mp3", songLength: '3:56', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Sign', artistId: 4, albumId: 4, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/microcosm/Sign.mp3', songLength: '3:56', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'UNION', artistId: 4, albumId: 4, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/microcosm/Union.mp3', songLength: '4:31', createdAt: new Date(), updatedAt: new Date(),
@@ -165,7 +165,7 @@ module.exports = {
         name: 'ENEMY', artistId: 4, albumId: 4, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/microcosm/Enemy.mp3', songLength: '4:13', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: "SOUL RED", artistId: 4, albumId: 4, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/microcosm/Soul+Red.mp3", songLength: '3:40', createdAt: new Date(), updatedAt: new Date(),
+        name: 'SOUL RED', artistId: 4, albumId: 4, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/microcosm/Soul+Red.mp3', songLength: '3:40', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'LUNA', artistId: 4, albumId: 4, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/microcosm/Luna.mp3', songLength: '4:46', createdAt: new Date(), updatedAt: new Date(),
@@ -190,7 +190,7 @@ module.exports = {
         name: 'B.Y.O.B', artistId: 5, albumId: 5, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/mesmerize/B.Y.O.B..mp3', songLength: '4:15', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Revenga', artistId: 5, albumId: 5, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/mesmerize/Revenga.mp3", songLength: '3:48', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Revenga', artistId: 5, albumId: 5, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/mesmerize/Revenga.mp3', songLength: '3:48', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'Cigaro', artistId: 5, albumId: 5, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/mesmerize/Cigaro.mp3', songLength: '2:11', createdAt: new Date(), updatedAt: new Date(),
@@ -211,7 +211,7 @@ module.exports = {
         name: 'Sad Statue', artistId: 5, albumId: 5, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/mesmerize/Sad+Statue.mp3', songLength: '3:25', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: "Old School Hollywood", artistId: 5, albumId: 5, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/mesmerize/Old+School+Hollywood.mp3", songLength: '2:56', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Old School Hollywood', artistId: 5, albumId: 5, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/mesmerize/Old+School+Hollywood.mp3', songLength: '2:56', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'Lost In Hollywood', artistId: 5, albumId: 5, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/mesmerize/Lost+In+Hollywood.mp3', songLength: '5:20', createdAt: new Date(), updatedAt: new Date(),
@@ -224,7 +224,7 @@ module.exports = {
         name: 'Jesus of Suburbia', artistId: 6, albumId: 6, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/american-idiot/Jesus+of+Suburbia.mp3', songLength: '9:08', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: "Holiday / Boulevard of Broken Dreams", artistId: 6, albumId: 6, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/american-idiot/Holiday+Boulevard+of+Broken+Dreams.mp3", songLength: '8:13', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Holiday / Boulevard of Broken Dreams', artistId: 6, albumId: 6, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/american-idiot/Holiday+Boulevard+of+Broken+Dreams.mp3', songLength: '8:13', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'Are We the Waiting / St Jimmy', artistId: 6, albumId: 6, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/american-idiot/Are+We+the+Waiting+St.+Jimmy.mp3', songLength: '5:38', createdAt: new Date(), updatedAt: new Date(),
@@ -252,7 +252,7 @@ module.exports = {
         name: 'Telephone Line', artistId: 7, albumId: 7, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/a-new-world-record/Telephone+Line.mp3', songLength: '4:40', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: "Rockaria!", artistId: 7, albumId: 7, songUrl: "https://crescendo-bucket.s3.us-west-1.amazonaws.com/a-new-world-record/Rockaria.mp3", songLength: '3:12', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Rockaria!', artistId: 7, albumId: 7, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/a-new-world-record/Rockaria.mp3', songLength: '3:12', createdAt: new Date(), updatedAt: new Date(),
       },
       {
         name: 'Mission (A World Record)', artistId: 7, albumId: 7, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/a-new-world-record/Mission+(A+World+Record).mp3', songLength: '4:25', createdAt: new Date(), updatedAt: new Date(),
@@ -274,56 +274,56 @@ module.exports = {
       },
 
       {
-        name: 'Pigs on the Wing 1', artistId: 8, albumId: 8, songUrl: '', songLength: '1:24', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Pigs on the Wing 1', artistId: 8, albumId: 8, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/animals/Pigs+On+The+Wing+(Part+One).mp3', songLength: '1:24', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Dogs', artistId: 8, albumId: 8, songUrl: '', songLength: '17:05', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Dogs', artistId: 8, albumId: 8, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/animals/Dogs.mp3', songLength: '17:05', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Pigs (Three Different Ones)', artistId: 8, albumId: 8, songUrl: '', songLength: '11:25', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Pigs (Three Different Ones)', artistId: 8, albumId: 8, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/animals/Pink+Floyd+-+Pigs+(Three+different+Ones).mp3', songLength: '11:25', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Sheep', artistId: 8, albumId: 8, songUrl: '', songLength: '10:19', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Sheep', artistId: 8, albumId: 8, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/animals/Sheep.mp3', songLength: '10:19', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Pigs on the Wing 2', artistId: 8, albumId: 8, songUrl: '', songLength: '1:26', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Pigs on the Wing 2', artistId: 8, albumId: 8, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/animals/Pigs+On+The+Wing+(Part+Two).mp3', songLength: '1:26', createdAt: new Date(), updatedAt: new Date(),
       },
 
       {
-        name: 'Papercut', artistId: 9, albumId: 9, songUrl: '', songLength: '3:04', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Papercut', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/Papercut.mp3', songLength: '3:04', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'One Step Closer', artistId: 9, albumId: 9, songUrl: '', songLength: '2:37', createdAt: new Date(), updatedAt: new Date(),
+        name: 'One Step Closer', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/One+Step+Closer.mp3', songLength: '2:37', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'With You', artistId: 9, albumId: 9, songUrl: '', songLength: '3:23', createdAt: new Date(), updatedAt: new Date(),
+        name: 'With You', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/With+You.mp3', songLength: '3:23', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Points of Authority', artistId: 9, albumId: 9, songUrl: '', songLength: '3:20', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Points of Authority', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/Points+Of+Authority.mp3', songLength: '3:20', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Crawling', artistId: 9, albumId: 9, songUrl: '', songLength: '3:28', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Crawling', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/Crawling.mp3', songLength: '3:28', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Runaway', artistId: 9, albumId: 9, songUrl: '', songLength: '3:03', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Runaway', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/Runaway.mp3', songLength: '3:03', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'By Myself', artistId: 9, albumId: 9, songUrl: '', songLength: '3:09', createdAt: new Date(), updatedAt: new Date(),
+        name: 'By Myself', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/By+Myself.mp3', songLength: '3:09', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'In The End', artistId: 9, albumId: 9, songUrl: '', songLength: '3:36', createdAt: new Date(), updatedAt: new Date(),
+        name: 'In The End', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/In+The+End.mp3', songLength: '3:36', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'A Place for My Head', artistId: 9, albumId: 9, songUrl: '', songLength: '3:04', createdAt: new Date(), updatedAt: new Date(),
+        name: 'A Place for My Head', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/A+Place+For+My+Head.mp3', songLength: '3:04', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Forgotten', artistId: 9, albumId: 9, songUrl: "", songLength: '3:14', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Forgotten', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/Forgotten.mp3', songLength: '3:14', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Cure for the Itch', artistId: 9, albumId: 9, songUrl: '', songLength: '2:37', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Cure for the Itch', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/Cure+For+The+Itch.mp3', songLength: '2:37', createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Pushing Me Away', artistId: 9, albumId: 9, songUrl: '', songLength: '3:11', createdAt: new Date(), updatedAt: new Date(),
+        name: 'Pushing Me Away', artistId: 9, albumId: 9, songUrl: 'https://crescendo-bucket.s3.us-west-1.amazonaws.com/hybrid-theory/Pushing+Me+Away.mp3', songLength: '3:11', createdAt: new Date(), updatedAt: new Date(),
       },
 
     ],
